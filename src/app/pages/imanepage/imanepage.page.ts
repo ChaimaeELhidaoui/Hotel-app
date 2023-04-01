283156d9759e693d0ce6ea815499a79fc26368e2
imane <imane.ziani1@ump.ac.ma>
2023-04-01 00:32:57 +0000

    imanes commit

--- src/app/pages/imanepage/imanepage.page.ts
+++ src/app/pages/imanepage/imanepage.page.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImanepagePage implements OnInit {
 
+ 
+  selectedCity!: string;
+  isDateTimePickerVisible = false;
+  selectedDate!: string;
+  departureDate!: string;
+  arrivalDate!: string;
   constructor() { }
 
   ngOnInit() {
+
+    
   }
+  showDateTimePicker() {
+    if (this.isDateTimePickerVisible == false){
+      this.isDateTimePickerVisible = true;
+    }
+    else {
+      this.isDateTimePickerVisible = false;
+    }
+    
+  
+}
+
 
 }
